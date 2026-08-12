cc1c37410121fcfe214f55f13e5c6ebbd2a164d0
JenuHari <144040830+Jenuhari@users.noreply.github.com>
2026-08-12 11:20:59 +0530

    Time: 0 ms (100.00%) | Memory: 20.4 MB (58.03%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -1,8 +1,9 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        n=len(nums)
-        for i in range(n):
-            for j in range(i+1, n):
-                if nums[i]+nums[j] == target:
-                    return [i, j]
-        
+        num_map = {}
+        for i, num in enumerate(nums):
+            compliment = target - num
+            if compliment in num_map:
+                return[num_map[compliment], i]
+            num_map[num] = i
+        return[]        
